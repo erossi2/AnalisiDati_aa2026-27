09201b4a1b71837f3e903509682f880885acdbf2
erossi2 <146084620+erossi2@users.noreply.github.com>
2026-08-18 12:39:29 +0200

    Add files via upload

---
+++ Introduction/shellexercise.sh
@@ -0,0 +1,31 @@
+echo "The echo command repeats whatever you type next in quotation marks, e.g. echo Hello world!"
+echo "Hello world!"
+echo "--------------------------"
+echo  "It also give the value of a variables using the dollar ($), e.g.$PATH and the path is"
+echo $PATH
+echo "--------------------------"
+echo "--------------------------"
+echo "comments are marked by the hash symbol (#)"
+#comments are marked by the hash symbol (#). This comment appears because the script uses "more"
+echo "--------------------------"
+echo "other useful commands to learn: 
+history : gives the history of the commands used
+tail : Reads a file from the end instead of the beginning
+ps : Shows open processes on the machine
+cd: change directory
+ls: list files
+rm: delete files
+pwd: get current directory
+mkdir: create directory
+touch: create file
+nano: update file"
+#
+#
+echo "--------------------------"
+echo "An interesting command is 'more' which reads a text file from start to finish, e.g.
+more shellexercise.sh. Now I will run the command: so you can see the output"
+echo "--------------------------"
+more shellexercise.sh
+echo "--------------------------"
+echo "More useful information can be found in https://www.coursera.org/articles/how-to-learn-linux (you can join it for free)"
+#
